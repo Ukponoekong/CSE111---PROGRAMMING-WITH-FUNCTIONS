923d156693df6e61f75a7808ee9d351d3b6b528f
UKPONOEKONG <ukponoekong@yahoo.com>
2026-01-21 18:04:35 +0100

    Writing functions to handle arithmetics.

---
+++ CSE111 - PROGRAMMING WITH FUNCTIONS/WEEK 02/CLASS WORK/practice.py
@@ -0,0 +1,18 @@
+#Practicing functions from writing simple codes.
+
+def add(first, second):
+    """
+    Add the second number and retirn the result
+    """
+    result = first + second
+    return result
+
+#Getting the varaible to be used as parameters from the users
+first = int(input("\nEnter an interger "))
+second = int(input("Enter an interger "))
+
+#Call the function and assign a name to it
+answer = add(first, second)
+print(f"Answer: {answer}")
+print()
+
